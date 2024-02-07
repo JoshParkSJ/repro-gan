e515b48557823a6c5378ba3d5509fe85595c7c2c
Joshua Park <50765702+JoshParkSJ@users.noreply.github.com>
2024-02-06 21:55:22 -0800

    standardize dblock and gblock

--- modules/gblock.py
+++ modules/gblock.py
@@ -1,61 +1,54 @@
-"""
-Implementation of convolutional blocks for generator.
-"""
 import torch.nn as nn
 import torch.nn.functional as F
 
 class GBlock(nn.Module):
-    r"""
-    Convolutional block for generator.
+    """
+    Block for generator.
 
-    Attributes:
+    Args:
         in_channels (int): The channel size of input feature map.
         out_channels (int): The channel size of output feature map.
-        upsample (bool): If True, upsamples the input feature map.
+        kernel_size (int): The size of the convolutional kernel.
+        stride (int, optional): The stride of the convolution. Default is 1.
+        padding (int, optional): The padding added to the input. Default is 0.
+        upsample (bool, optional): If True, upsample the input feature map. Default is False.
+        use_reflection_pad (bool, optional): If True, uses reflection padding. Default is False.
     """
     def __init__(self,
                  in_channels,
                  out_channels,
-                 kernel_size=3,
+                 kernel_size,
                  stride=1,
-                 padding=1,
-                 activation=True,
-                 batchNorm=True,
+                 padding=0,
                  upsample=False,
-                 reflectionPad=False,
-                 ):
+                 use_reflection_pad=False):
         super().__init__()
-        self.padding = padding
-        self.activation = activation
-        self.batchNorm = batchNorm
-        self.upsample = upsample
-        self.reflectionPad = reflectionPad
-
-        if (reflectionPad):
-            self.convLayer = nn.Conv1d(in_channels, out_channels, kernel_size, stride, padding)
-        else:
-            self.convLayer = nn.Conv1d(in_channels, out_channels, kernel_size, stride)
-            self.paddingLayer = nn.ReflectionPad1d((padding ,padding))
-
+        self.use_reflection_pad = use_reflection_pad
         self.batchNormLayer = nn.BatchNorm1d(out_channels)
-        self.upsampleLayer = nn.Upsample(scale_factor=2)
-        self.activationLayer = nn.LeakyReLU(0.2)
+        self.activation = nn.LeakyReLU(0.2)
+        self.upsample = upsample
 
-        nn.init.normal_(self.convLayer.weight.data, 0.0, 0.02)
+        # reflection padding
+        self.conv_no_pad = nn.Conv1d(in_channels, out_channels, kernel_size, stride)
+        self.reflection_padding = nn.ReflectionPad1d((padding,padding))
+        nn.init.normal_(self.conv_no_pad.weight.data, 0.0, 0.02)
+
+        # zero padding
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, stride, padding)
+        nn.init.normal_(self.conv.weight.data, 0.0, 0.02)
 
     def forward(self, x):
-        y = self.convLayer(x)
-
-        if (self.upsample):
-            y = self.upsampleLayer(y)
-
-        if (self.reflectionPad):
-            y = self.paddingLayer(y)
-
-        if (self.batchNorm):
-            y = self.batchNormLayer(y)
-
-        if (self.activation):
-            y = self.activationLayer(y)
-
-        return y
+        h = x
+        if self.upsample:
+            h = F.interpolate(h,
+                    scale_factor=2,
+                    mode='linear',
+                    align_corners=False)
+        if self.use_reflection_pad:
+            h = self.reflection_padding(h)
+            h = self.conv_no_pad(h)
+        else:
+            h = self.conv(x)
+        h = self.batchNormLayer(h)
+        h = self.activation(h)
+        return h
